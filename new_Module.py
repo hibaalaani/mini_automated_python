cd6aceb12d010000c32735e124260e5aa5ca07c6
hibaalaani <alaani.hiba@gmail.com>
2024-06-26 14:42:56 +0200

    update-2

--- new_Module.py
+++ new_Module.py
@@ -1,7 +1,7 @@
 from stringcolor import cs 
 """ not work in window """
 
-
+# mini code to handle using the module stringcolor
 
 def print_color(string , color="red"):
     print(cs(string , color))
